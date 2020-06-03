9ffd27f79ff9242ae785faff04ae9d643f4f8b76
Brown Nguyễn <brownnguyen@users.noreply.github.com>
2020-06-04 01:41:27 +0700

    change

--- src/Redux/rootReducer.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from 'redux';
-import { CourseReducer } from './CouseReducer.js';
-import { CartReducer } from './CartReducer.js';
-import { UserReducer } from './UserReducer.js';
-export const rootReducer = combineReducers({
-    CourseReducer, CartReducer, UserReducer
-})
--- src/Services/index.js
+++
@@ -1,3 +0,0 @@
-import UserServices from "./User";
-
-export const userServices = new UserServices();
